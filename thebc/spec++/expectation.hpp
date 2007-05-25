9fec059842f69402275a0e08f65f487c653813a3
Fredrik Eriksson <fredrik.eriksson@thebc.se>
2007-05-25 19:13:33 +0000

    Renamed expectationv2 to expectation. Updated expectation.hpp so that it uses the new expectation API.

--- thebc/spec++/expectation.hpp
+++ thebc/spec++/expectation.hpp
@@ -2,14 +2,11 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-
 // ---------------------------------------------------------------------------
 
 #ifndef EXPECTATION_HPP
 #define EXPECTATION_HPP
 
-#include <thebc/spec++/expectation/basic.hpp>
-#include <thebc/spec++/expectation/value.hpp>
-#include <thebc/spec++/expectation/exception.hpp>
+#include <thebc/spec++/expectation/actual.hpp>
 
 #endif // EXPECTATION_HPP
